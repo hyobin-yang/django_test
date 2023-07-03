75ff2157ab967306060f98fa5430ec0beccbfbbf
hyobin-yang <hyobin3026@naver.com>
2023-07-03 18:27:56 +0900

    fourth commit

--- modelproject/settings.py
+++ modelproject/settings.py
@@ -82,13 +82,23 @@ WSGI_APPLICATION = "modelproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '010326',
+        'HOST': 'svc.sel4.cloudtype.app',
+        'PORT': '31138'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
